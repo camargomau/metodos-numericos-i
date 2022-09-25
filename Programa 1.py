a4d4f3bfb198fe11aec96029b95c7f1fe0eac7a5
Luis Mauricio Camargo <52770775+Psycake@users.noreply.github.com>
2022-09-25 12:32:28 -0500

    Portada y más
    - Se agregó la portada (aún faltan dos nombres)
    - Se agregó una forma de regresar al menú de funciones cuando ya se seleccionó una, en caso de que la función no tenga raíces y el usuario se atore (bisección y posición falsa)
    - Cambios menores en la impresión de ciertas cosas

--- Programa 1.py
+++ Programa 1.py
@@ -13,6 +13,27 @@ def clear_screen():
     else:
         _ = os.system('cls')
 
+def portada():
+    clear_screen()
+    print("Universidad Nacional Autónoma de México")
+    print("Facultad de Estudios Superiores Acatlán\n")
+
+    print("---------------------------------------\n")
+
+    print("Programa 1:\nMétodos de Solución de Ecuaciones No Lineales\n")
+
+    print("Camargo Badillo Luis Mauricio")
+    print("Leon Valdés Duhart Guillermo Arturo")
+    print("???")
+    print("???\n")
+
+    print("Métodos Numéricos I")
+    print("Grupo 1301\n")
+
+    print("---------------------------------------\n")
+
+    input("Presiona enter para continuar")
+
 #########
 # Menús #
 #########
@@ -45,7 +66,7 @@ def menu_metodos():
             break
         else:
             print(
-                f"\nIntroduzca un número entre 0 y {len(opciones_metodos.keys())-1}")
+                f"\nIntroduzca un número entre 0 y {len(opciones_metodos.keys())-1}.")
 
     if eleccion_metodo != 0:
         menu_funciones()
@@ -54,14 +75,14 @@ def menu_funciones():
     global func, eleccion_funcion
     opciones_funciones = {
         1: "x**2 * cos(x) - 2*x",
-        2: "(((6-2)/x**2) * E**(x+2))/(4+1)",
+        2: "(((6-2)/x**2) * E**(x+2)) / (4+1)",
         3: "x**3 - 3*sin(x**2) + 1",
         4: "x**3 + 6*(x**2) + 9.4*x + 2.5",
 
         # Funciones de los ejercicios del portafolio
-        5: "((50 + (37.49/x**2)) * (x - 0.197)) - (0.08205*348.15)",
-        6: "x * ((15*x)/(15+2*x))**(2/3) - ((0.015*20)/(15*sqrt(0.001)))",
-        7: "(((2+0.4*x**2)/2.4)**3.5 - 1)/(0.7*x**2*(-0.383)) - (sqrt(1-x**2)+(((x**2*(-0.383))/2)/(1+sqrt(1-x**2))))**(-1)",
+        #5: "((50 + (37.49/x**2)) * (x - 0.197)) - (0.08205*348.15)",
+        #6: "x * ((15*x)/(15+2*x))**(2/3) - ((0.015*20)/(15*sqrt(0.001)))",
+        #7: "(((2+0.4*x**2)/2.4)**3.5 - 1)/(0.7*x**2*(-0.383)) - (sqrt(1-x**2)+(((x**2*(-0.383))/2)/(1+sqrt(1-x**2))))**(-1)",
 
         0: "Regresar al menú de métodos"
     }
@@ -101,6 +122,35 @@ def menu_funciones():
 # Métodos #
 ###########
 
+# Introducción de iteraciones máximas, tolerancia y sus checks
+def maxIt_toler_checks():
+    while True:
+        try:
+            max_it = int(input("• Máximo de iteraciones: "))
+        except:
+            print("\nIntroduzca un número entero.\n")
+            continue
+
+        if max_it > 0:
+            break
+        else:
+            print("\nIntroduzca un número entero mayor que 0.\n")
+
+    # Tolerancia y sus checks
+    while True:
+        try:
+            toler = float(input("• Tolerancia (error relativo): "))
+        except:
+            print("\nIntroduzca un número real.\n")
+            continue
+
+        if (toler > 0) and (toler < 1):
+            break
+        else:
+            print("\nIntroduzca un valor en (0, 1).\n")
+
+    return max_it, toler
+
 # Según las elecciones del usuario en el menú, utilizar el método deseado
 def trigger_metodo():
     global it, deriv
@@ -118,9 +168,16 @@ def trigger_metodo():
 
         # Intervalo y sus checks
         while True:
+            # Como pueden haber funciones sin ninguna raíz, dar la opción de regresar
+            print("(introduzca una 'x' si desea regresar al menú de funciones)")
+
+            a = input("• Valor de a (extremo izquierdo del intervalo): ")
+            if (a == 'x') or (a == 'X'):
+                menu_funciones()
+
             try:
-                a = float(
-                    input("• Valor de a (extremo izquierdo del intervalo): "))
+
+                a = float(a)
                 b = float(input("• Valor de b (extremo derecho): "))
             except:
                 print("\nIntroduzca números reales.\n")
@@ -137,7 +194,8 @@ def trigger_metodo():
             try:
                 menor_cero = (fa * fb) < 0 
             except:
-                print(f"\nLa función evaluada en {a} o en {b} da como resultado un número complejo. Intenta con otro intervalo.\n")
+                print(
+                    f"\nLa función evaluada en {a} o en {b} da como resultado un número complejo. Intenta con otro intervalo.\n")
                 continue
 
             if menor_cero:
@@ -148,31 +206,8 @@ def trigger_metodo():
                 print(
                     f"Es posible que no haya una raíz en el intervalo [{a}, {b}]. Intenta con otro intervalo.\n")
 
-        # Iteraciones máximas y sus checks
-        while True:
-            try:
-                max_it = int(input("• Máximo de iteraciones: "))
-            except:
-                print("\nIntroduzca un número entero.\n")
-                continue
-
-            if max_it > 0:
-                break
-            else:
-                print("\nIntroduzca un número entero mayor que 0\n")
-
-        # Tolerancia y sus checks
-        while True:
-            try:
-                toler = float(input("• Tolerancia (error relativo): "))
-            except:
-                print("\nIntroduzca un número real.\n")
-                continue
-
-            if (toler > 0) and (toler < 1):
-                break
-            else:
-                print("\nIntroduzca un valor en (0, 1).\n")
+        # Iteraciones máximas, tolerancia y sus checks
+        max_it, toler = maxIt_toler_checks()
 
         # Todo correcto si se llegó hasta aquí
         # Encabezado de la tabla
@@ -189,30 +224,36 @@ def trigger_metodo():
         raiz, err_rel = biseccion_posFalsa(a, b, 0, max_it, toler)
         if err_rel <= toler:
             print(
-                f"\nSe encontró una raíz en el intervalo [{a}, {b}] con un error relativo menor o igual a {toler}")
-            print(f"La raíz encontrada fue {raiz}")
+                f"\nSe encontró una raíz en el intervalo [{a}, {b}] con un error relativo menor o igual a {toler}:")
+            print(f"- La raíz encontrada fue {raiz}")
             print(
-                f"Se necesitó de {it} iteraciones y la raíz tiene un error relativo de {err_rel}")
-            input("\nPresiona enter para continuar")
+                f"- Se necesitó de {it} iteraciones y la raíz tiene un error relativo de {err_rel}")
+            input("\nPresiona enter para continuar.")
         else:
-            print(f"\nSe alcanzaron las {it} iteraciones máximas")
+            print(f"\nSe alcanzaron las {it} iteraciones máximas:")
             print(
-                f"No se pudo encontrar una raíz en el intervalo [{a}, {b}] con un error relativo menor a {toler}")
+                f"- No se pudo encontrar una raíz en el intervalo [{a}, {b}] con un error relativo menor a {toler}")
             print(
-                f"El valor más preciso encontrado fue {raiz}, que tiene un error relativo de {err_rel}")
-            print("Puede volver a intentar encontrar una raíz, esta vez con más iteraciones, otra tolerancia u otro intervalo")
-            input("\nPresiona enter para continuar")
+                f"- El valor más preciso encontrado fue {raiz}, que tiene un error relativo de {err_rel}")
+            print("Puede volver a intentar encontrar una raíz, esta vez con más iteraciones, otra tolerancia u otro intervalo.")
+            input("\nPresiona enter para continuar.")
 
     # Newton
     elif eleccion_metodo == 3:
-        print(f"Elegiste el método de Newton.")
+        print(f"Elegiste el método de Newton.\n")
 
         deriv = diff(func)
 
         # x0 y sus checks
         while True:
+            print("(introduzca una 'x' si desea regresar al menú de funciones)")
+
+            x0 = input("• Valor inicial (x_0): ")
+            if (x0 == 'x') or (x0 == 'X'):
+                menu_funciones()
+
             try:
-                x0 = float(input("• Valor inicial (x_0): "))
+                x0 = float(x0)
             except:
                 print("\nIntroduzca un número real.\n")
                 continue
@@ -230,31 +271,8 @@ def trigger_metodo():
                 print(
                     f"\nLa derivada evaluada en {x0} es igual a 0. Intenta con otro valor.\n")
 
-        # Iteraciones máximas y sus checks
-        while True:
-            try:
-                max_it = int(input("• Máximo de iteraciones: "))
-            except:
-                print("\nIntroduzca un número entero.\n")
-                continue
-
-            if max_it > 0:
-                break
-            else:
-                print("\nIntroduzca un número entero mayor que 0\n")
-
-        # Tolerancia y sus checks
-        while True:
-            try:
-                toler = float(input("• Tolerancia (error relativo): "))
-            except:
-                print("\nIntroduzca un número real.\n")
-                continue
-
-            if (toler > 0) and (toler < 1):
-                break
-            else:
-                print("\nIntroduzca un valor en (0, 1).\n")
+        # Iteraciones máximas, tolerancia y sus checks
+        max_it, toler = maxIt_toler_checks()
 
         # Todo correcto si se llegó hasta aquí
         # Encabezado de la tabla
@@ -271,28 +289,34 @@ def trigger_metodo():
         raiz, err_rel = newton(x0, max_it, toler)
         if err_rel <= toler:
             print(
-                f"\nSe encontró una raíz a partir del punto inicial {x0} con un error relativo menor o igual a {toler}")
-            print(f"La raíz encontrada fue {raiz}")
+                f"\nSe encontró una raíz a partir del punto inicial {x0} con un error relativo menor o igual a {toler}:")
+            print(f"- La raíz encontrada fue {raiz}")
             print(
-                f"Se necesitó de {it} iteraciones y la raíz tiene un error relativo de {err_rel}")
-            input("\nPresiona enter para continuar")
+                f"- Se necesitó de {it} iteraciones y la raíz tiene un error relativo de {err_rel}")
+            input("\nPresiona enter para continuar.")
         else:
-            print(f"\nSe alcanzaron las {it} iteraciones máximas")
+            print(f"\nSe alcanzaron las {it} iteraciones máximas:")
             print(
-                f"No se pudo encontrar una raíz a partir del punto inicial {x0} con un error relativo menor a {toler}")
+                f"- No se pudo encontrar una raíz a partir del punto inicial {x0} con un error relativo menor a {toler}")
             print(
-                f"El valor más preciso encontrado fue {raiz}, que tiene un error relativo de {err_rel}")
-            print("Puede volver a intentar encontrar una raíz, esta vez con más iteraciones, otra tolerancia u otro valor inicial")
-            input("\nPresiona enter para continuar")
+                f"- El valor más preciso encontrado fue {raiz}, que tiene un error relativo de {err_rel}")
+            print("Puede volver a intentar encontrar una raíz, esta vez con más iteraciones, otra tolerancia u otro valor inicial.")
+            input("\nPresiona enter para continuar.")
 
     # Secante
     elif eleccion_metodo == 4:
-        print(f"Elegiste el método de la secante.")
+        print(f"Elegiste el método de la secante.\n")
 
-        # Intervalo y sus checks
+        # Valores iniciales y sus checks
         while True:
+            print("(introduzca una 'x' si desea regresar al menú de funciones)")
+
+            x0 = input("• Valor inicial (x_0): ")
+            if (x0 == 'x') or (x0 == 'X'):
+                menu_funciones()
+
             try:
-                x0 = float(input("• Valor de x_0: "))
+                x0 = float(x0)
                 x1 = float(input("• Valor de x_1: "))
             except:
                 print("\nIntroduzca números reales.\n")
@@ -308,31 +332,8 @@ def trigger_metodo():
 
             break
 
-        # Iteraciones máximas y sus checks
-        while True:
-            try:
-                max_it = int(input("• Máximo de iteraciones: "))
-            except:
-                print("\nIntroduzca un número entero.\n")
-                continue
-
-            if max_it > 0:
-                break
-            else:
-                print("\nIntroduzca un número entero mayor que 0\n")
-
-        # Tolerancia y sus checks
-        while True:
-            try:
-                toler = float(input("• Tolerancia (error relativo): "))
-            except:
-                print("\nIntroduzca un número real.\n")
-                continue
-
-            if (toler > 0) and (toler < 1):
-                break
-            else:
-                print("\nIntroduzca un valor en (0, 1).\n")
+        # Iteraciones máximas, tolerancia y sus checks
+        max_it, toler = maxIt_toler_checks()
 
         # Todo correcto si se llegó hasta aquí
         # Encabezado de la tabla
@@ -349,19 +350,19 @@ def trigger_metodo():
         raiz, err_rel = secante(x0, x1, max_it, toler)
         if err_rel <= toler:
             print(
-                f"\nSe encontró una raíz a partir de los puntos iniciales {x0} y {x1} con un error relativo menor o igual a {toler}")
-            print(f"La raíz encontrada fue {raiz}")
+                f"\nSe encontró una raíz a partir de los puntos iniciales {x0} y {x1} con un error relativo menor o igual a {toler}:")
+            print(f"- La raíz encontrada fue {raiz}")
             print(
-                f"Se necesitó de {it} iteraciones y la raíz tiene un error relativo de {err_rel}")
-            input("\nPresiona enter para continuar")
+                f"- Se necesitó de {it} iteraciones y la raíz tiene un error relativo de {err_rel}")
+            input("\nPresiona enter para continuar.")
         else:
-            print(f"\nSe alcanzaron las {it} iteraciones máximas")
+            print(f"\nSe alcanzaron las {it} iteraciones máximas:")
             print(
-                f"No se pudo encontrar una raíz a partir de los puntos iniciales {x0} y {x1} con un error relativo menor a {toler}")
+                f"- No se pudo encontrar una raíz a partir de los puntos iniciales {x0} y {x1} con un error relativo menor a {toler}")
             print(
-                f"El valor más preciso encontrado fue {raiz}, que tiene un error relativo de {err_rel}")
-            print("Puede volver a intentar encontrar una raíz, esta vez con más iteraciones, otra tolerancia u otro valor inicial")
-            input("\nPresiona enter para continuar")
+                f"- El valor más preciso encontrado fue {raiz}, que tiene un error relativo de {err_rel}")
+            print("Puede volver a intentar encontrar una raíz, esta vez con más iteraciones, otra tolerancia u otro valor inicial.")
+            input("\nPresiona enter para continuar.")
 
     fin_metodo()
 
@@ -372,7 +373,7 @@ def biseccion_posFalsa(a, b, anterior, max_it, toler):
     fa = func.evalf(subs={x: a})
     fb = func.evalf(subs={x: b})
 
-    assert (fa * fb) < 0, "f(a) y f(b) deberían tener signos diferentes; en teoría nunca debería imprimirme"
+    assert (fa * fb) < 0, "f(a) y f(b) deberían tener signos diferentes; en teoría nunca debería imprimirme."
 
     # Calcular el punto medio, según el método seleccionado
     # Bisección
@@ -411,8 +412,8 @@ def newton(xk, max_it, toler):
 
     if (fxk == 0) or (dxk == 0) or (xkp1 == 0):
         print("\n| {105} |".format(
-            "Valor inicial desafortunado (división entre 0). Intenta con otro"))
-        input("\nPresiona enter para continuar")
+            "Valor inicial desafortunado (división entre 0). Intenta con otro."))
+        input("\nPresiona enter para continuar.")
         trigger_metodo()
 
     err_rel = abs(xkp1 - xk)/abs(xkp1)
@@ -477,4 +478,5 @@ def fin_metodo():
 # Principal #
 #############
 
+portada()
 menu_metodos()
